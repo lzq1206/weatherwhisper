55980a79cd59623afc8243a72daa0a3b641a341c
Your Name <you@example.com>
2026-03-30 12:20:07 +0800

    fix: explicit state casting in InterpolationLayer and set base path in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weatherwhisper/',
   server: {
     port: 8080,
     open: true,
